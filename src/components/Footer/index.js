0ec12a347e943184cd9798cc1015a39640708b03
Elijah Micho <olum.micho@gmail.com>
2021-02-22 15:20:59 -0700

    created footer component, will finish styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,11 +5,39 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footerLogo">
-                <a>
+                <a href="#">
                     <span>justrecipes</span>
                     <i className="fa fa-utensils"></i>
                 </a>
             </div>
+
+            <nav className="footerSections">
+                <div className="magazinesCTA">
+                    <h3>Magazines &amp; More</h3>
+                    <a><img/></a>
+                </div>
+                <div className="learnMoreCTA">
+                    <h3>Learn More</h3>
+                    <ul>
+                        <li>Customer Service</li>
+                        <li>Content Licensing</li>
+                        <li>Apps</li>
+                    </ul>
+                </div>
+                <div className="connectCTA">
+                    <h3>Connect</h3>
+                    <span>Follow Us</span>
+
+                    <ul>
+                        <li><a><i className="fa fa-facebook"></i></a></li>
+                        <li><a><i className="fa fa-pintrest"></i></a></li>
+                        <li><a><i className="fa fa-twitter"></i></a></li>
+                        <li><a><i className="fa fa-instagram"></i></a></li>
+                    </ul>
+
+                    <span>Subscribe</span>
+                </div>
+            </nav>
         </footer>
     );
 };
